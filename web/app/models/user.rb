6b426dc7537b22ce46b07a2f96db39590dc8f54d
SLEAZOIDS <vegetarian304@gmail.com>
2019-06-17 18:57:27 +0900

    client側実装

---
+++ web/app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
+ 
+  def self.find_for_mock(auth)
+     parameters = { 
+       name:     auth.info.name,
+       email:    auth.info.email,
+       provider: auth.provider,
+       uid:      auth.uid,
+       token:    auth.credentials.token,
+       password: Devise.friendly_token[0, 20],
+       raw: auth.extra.to_json
+     }   
+     user = User.find_by(uid: auth.uid)
+     return update_mock!(user, parameters) if user
+ 
+     User.create(parameters)
+   end 
+ 
+   def self.update_mock!(user, parameters)
+     user.update(parameters)
+     user
+   end 
+ end
